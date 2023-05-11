b9d6feb9d6a4de27c0acb0a33e96e9670bc15d7e
hyunmijin <hunmi961119@gmail.com>
2023-05-11 22:04:14 +0900

    23.05.11

--- web2023-PHP/include/footer.php
+++
@@ -1,7 +0,0 @@
-<footer id="footer">
-    <div class="footer__inner btStyle mb100 container">
-        <div>copyright@2023 hyunmi</div>
-        <div>blog by hyunmi</div>
-    </div>
-</footer>
-<!-- //footer -->
