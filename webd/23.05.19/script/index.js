1d7de1f2c11cbdd256b2a051ce80129b09ecd966
jinhyunmi <hunmi961119@gmail.com>
2023-05-19 16:47:30 +0900

    23.05.19

--- webd/23.05.19/script/index.js
+++ webd/23.05.19/script/index.js
@@ -1,30 +1,27 @@
 $(function () {
-    // 메뉴
-    $(".nav > ul > li").mouseover(function () {
-        $(".nav > ul > li > ul").stop().fadeIn(400);
-        $("#header .container").addClass("on");
+    $(".nav > ul >  li").mouseover(function () {
+        $(this).find(".submenu").stop().slideleft(200);
     });
-    $(".nav > ul > li").mouseout(function () {
-        $(".nav > ul > li > ul").stop().fadeOut(100);
-        $("#header .container").removeClass("on");
+    $(".nav > ul >  li").mouseout(function () {
+        $(this).find(".submenu").stop().slideUp(200);
     });
 
     // 슬라이드
     let currentIndex = 0;
     const $sliderWrap = $(".sliderWrap");     // 이미지 부모 : 움직이는 영역
     const $slider = $(".slider");             //각각의 이미지
-    const $sliderWidth = $slider.width();     //이미지 가로값
-    console.log($sliderWidth)
+    const $sliderHight = $slider.highth();     
+    console.log($sliderHight)
     $sliderWrap.append($slider.first().clone(true));     //첫번째 이미지를 복사해서 마지막에 추간
 
 
     setInterval(function () {
         currentIndex++;     // 현재 이미지를 1씩 증가
-        $sliderWrap.animate({ marginTop: (-$sliderWidth + 400) * currentIndex }, 600)
+        $sliderWrap.animate({ marginLeft: (-$sliderHight + 350) * currentIndex }, 700)
 
         if (currentIndex == $slider.length) {
             setTimeout(function () {
-                $sliderWrap.animate({ marginTop: 0 }, 0);
+                $sliderWrap.animate({ marginLeft: 0 }, 0);
                 currentIndex = 0;
             }, 700)
         }
